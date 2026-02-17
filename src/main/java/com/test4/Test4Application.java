7fe82bb02d38385df2dce71cf6c7ce6d11f874bb
Sandy9902 <Sandynlaz@gmail.com>
2026-02-17 18:44:50 +0530

    Update Test4Application.java

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -7,6 +7,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
+		String name = "mike";
+		int x = 40;
+		boolean isPresent = true;
+		int y = 50;
+		char c = 'r';
+		int y = 50;
+		double d =20.3;
+		boolean isPresent = false;
+		float f = 40.4f;
 		SpringApplication.run(Test4Application.class, args);
 	}
 
